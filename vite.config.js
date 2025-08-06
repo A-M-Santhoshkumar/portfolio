043716faea5eb414f761195b48d2be8293f100c1
Santhoshkumar <a.m.santhoshkumar02@gmail.com>
2025-08-06 19:42:07 +0530

    new some changs

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ For GitHub Pages subfolder
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 1500,
-  },
-});
+})
